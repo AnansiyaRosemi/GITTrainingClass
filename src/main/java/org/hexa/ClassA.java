9edde12460e7c47900d5a1a23c4cd62835f328e8
AnansiyaRosemi <anansiyarosemi@gmail.com>
2023-04-05 11:26:24 +0530

    First Time Push

---
+++ src/main/java/org/hexa/ClassA.java
@@ -0,0 +1,24 @@
+package org.hexa;
+
+public class ClassA {
+
+	private void test1() {
+	
+System.out.println("test1 ");
+	}
+	
+	private void test2() {
+		
+		System.out.println("test2 ");
+
+	}
+	
+	private void test3() {
+	
+		System.out.println("test3 ");
+	}
+	private void test4() {
+	
+		System.out.println("test4 ");
+	}
+}
